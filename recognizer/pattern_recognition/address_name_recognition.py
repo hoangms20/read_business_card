7b23171ba7504bb9baf11bbd77a3396d0f47f808
hoangms20 <hoangms20@gmail.com>
2021-06-05 23:01:11 +0700

    Update address_name_recognition.py

--- recognizer/pattern_recognition/address_name_recognition.py
+++ recognizer/pattern_recognition/address_name_recognition.py
@@ -13,7 +13,7 @@ with open(path + '/../data/address_token') as add_tokens:
 
 
 def is_address(line):
-    lower_line = line.decode('utf8').lower()
+    lower_line = line.encode().decode('utf8').lower()
     for add_token in common_address_token:
         if lower_line.find(add_token) >= 0:
             return True
