67811cefe2d9bbd546c636df554bb8c092a7dbf0
hoangms20 <hoangms20@gmail.com>
2021-06-07 03:48:03 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,8 @@
 from tkinter import *
 
-lay=[]
 root = Tk()
 root.geometry('300x400+100+50')
 
-def create():
+some_widget = Toplevel(root)
 
-    top = Toplevel()
-    lay.append(top)
-
-    top.title("Main Panel")
-    top.geometry('500x500+100+450')
-    msg = Message(top, text="Show on Sub-panel",width=100)
-    msg.pack()
-
-    def exit_btn():
-
-        top.destroy()
-        top.update()
-
-    btn = Button(top,text='EXIT',command=exit_btn)
-    btn.pack()
-
-
-Button(root, text="Click me,Create a sub-panel", command=create).pack()
-mainloop()
+top = some_widget.winfo_toplevel()
