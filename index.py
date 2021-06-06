3d102192e718d54a32d6a83d0b045aea91cac816
hoangms20 <hoangms20@gmail.com>
2021-06-06 19:30:12 +0700

    Update index.py

--- index.py
+++ index.py
@@ -27,100 +27,104 @@ import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Admin\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-
-root = Tk()
-  
-# fixing geometry of GUI
-root.geometry('800x500')        
-root.maxsize(1000, 500)
-root.minsize(600, 500)
-root.title('Visiting card scanner')
-
 #upload file
 def upload_file():
+    global filename, img
+    global start, last
     filename = filedialog.askopenfilename(initialdir=os.getcwd(),
         title="Select Image File", 
         filetypes=(("JPG File", "*.jpg"), ("PNG File", "*.png"), ("All File", "*.*")))
-    img = ImageTk.PhotoImage(Image.open(filename))
-    root1 = Toplevel()
-    root1.title("Image")
-    print(filename)
-    img2 = cv2.imread(filename)
-    gray = cv2.cvtColor(img2, code = cv2.COLOR_BGR2GRAY)
-    text = pytesseract.image_to_string(gray)
-    t.delete(1.0, END)
-    t.insert(1.0, text)
-    Label(root1, image=img).pack()
-    root1.mainloop() 
 
+    if filename == "":
+        t.delete(1.0, END)
+        tmsg.showwarning(title = 'Alert!', message = 'Please provide proper formatted image')
+        return
+    else:
+        image_size = 800
+        img = Image.open(filename)
+        orig_width, orig_height = img.size
+        scale = (orig_height / orig_width) * image_size
+        img = ImageTk.PhotoImage(img.resize((image_size, int(scale)), Image.ANTIALIAS))
+        root1 = Toplevel()
+        root1.geometry('800x800')        
+        root1.title("Image upload")
+        Label(root1, image=img).pack()
+        p_label_var.set("Image uploaded successfully")
+        l.config(fg = '#0CDD19')
 
-    #if	filename == '':
-
-def convert():
-    # gray = cv2.cvtColor(src = filename, code = cv2.COLOR_BGR2GRAY)
-	# text = pytesseract.image_to_string(filename)
-	# t.delete(1.0, END)
-	# t.insert(1.0, text)
-	# root1 = Toplevel()
-	# root1.title('Uploaded image')
-	# img1 = ImageTk.PhotoImage(Image.open(filename))
-	# Label(root1, image=img1).pack()
-	# root1.mainloop()
     return
 
 
-# Menu bar and navigation tab creation
-mainmenu = Menu(root)
-mainmenu.config(font = ('Times', 29))
-  
-m1 = Menu(mainmenu, tearoff = 0)
-m1.add_command(label = 'Scan/Upload Visiting or Bussiness cards and get all the text of cards',
-               font = ('Times', 13))
-root.config(menu = mainmenu)
-mainmenu.add_cascade(label = 'Aim', menu = m1)
-  
-Label(text = 'Visiting card scanner', bg = '#FAD2B8',
-      fg = '#39322D', font = ('Times', 18)).pack(fill = 'x')
-Label(text = 'Python GUI', bg = '#FAD2B8', fg ='#39322D', font=(
-    'Times New Roman', 12, 'italic')).pack(fill='x')
-  
-f1 = Frame()
-f1.config(bg='white')
-Label(f1, text='Browse photo to upload', width=20,
-      font=('Times', 15), bg='white').pack(side='left')
-Label(f1, text='format: png/jpeg', bg='white',
-      width=30).pack(side='right', padx=5)
-Button(f1, text='Upload card', bg='#F58D4B', font=('Times', 15),
-       width=70, command=upload_file).pack(side='right')
-f1.pack(pady=10, fill='x')
-p_label_var = StringVar()
-p_label_var.set('Please upload an image to scan')
-l = Label(textvariable=p_label_var, fg='red', bg='white')
-l.pack()
-  
-t = Text(root, height='9', font=('Times', 13))
-t.pack(side='bottom', fill='x')
-t.insert(1.0, 'Text of converted card will be shown here...', END)
-c_label_var = StringVar()
-c_label_var.set('Ready for conversion')
-c_label = Label(textvariable=c_label_var)
-c_label.pack(side='bottom', anchor='w')
-Button(root, text='Scan and Convert', bg='#F58D4B', font=('Times', 15),
-       width=70, command=convert).pack(pady='10', side='bottom')
-root.mainloop()
+def convert():
+    contact = recognize_contact(filename)
+    print((' ' * 30))
+    print((' * ' * 30))
+    print((jsonpickle.encode(contact)))
+    t.delete(1.0, END)
+    t.insert(1.0, (jsonpickle.encode(contact)))
 
 
-# if __name__ == "__main__":
-#     # app.run()
-#     sample_path = 'samples/bc_ref/r_13.jpg'
-#     #sample_path = 'card.png'
-#     # extract_2(sample_path)
-#     # sample_path = 'samples/BC_5.jpg'
 
-#     # sample_path = 'business_cards/Droid/010.jpg'
-#     contact = recognize_contact(sample_path)
-#     print((' ' * 30))
-#     print((' * ' * 30))
-#     print((jsonpickle.encode(contact)))
-#     print((' * ' * 30))
+if __name__ == "__main__":
+    root = Tk()
+  
+    # fixing geometry of GUI
+    root.geometry('800x500')        
+    root.maxsize(1000, 500)
+    root.minsize(600, 500)
+    root.title('Visiting card scanner')
+
+    # Menu bar and navigation tab creation
+    mainmenu = Menu(root)
+    mainmenu.config(font = ('Times', 29))
+    
+    m1 = Menu(mainmenu, tearoff = 0)
+    m1.add_command(label = 'Scan/Upload Visiting or Bussiness cards and get all the text of cards',
+                font = ('Times', 13))
+    root.config(menu = mainmenu)
+    mainmenu.add_cascade(label = 'Aim', menu = m1)
+    
+    #title
+    Label(text = 'Visiting card scanner', bg = '#FAD2B8',
+        fg = '#39322D', font = ('Times', 18)).pack(fill = 'x')
+    Label(text = 'Python GUI', bg = '#FAD2B8', fg ='#39322D', font=(
+        'Times New Roman', 12, 'italic')).pack(fill='x')
+    
+    # create frame
+    f1 = Frame()
+    f1.config(bg='white')
+    Label(f1, text='Browse photo to upload', width=20,
+        font=('Times', 15), bg='white').pack(side='left')
+    Label(f1, text='format: png/jpeg', bg='white',
+        width=30).pack(side='right', padx=5)
+
+    #button upload
+    Button(f1, text='Upload card', bg='#F58D4B', font=('Times', 15),
+        width=70, command=upload_file).pack(side='right')
+    f1.pack(pady=10, fill='x')
+
+    #label upload
+    p_label_var = StringVar()
+    p_label_var.set('Please upload an image to scan')
+    l = Label(textvariable=p_label_var, fg='red', bg='white')
+    l.pack()
+    
+    #show result
+    t = Text(root, height='2', font=('Times', 13))
+    t.pack(side='bottom', fill='x')
+    t.insert(1.0, 'Text of converted card will be shown here...', END)
+
+    #convert lablel
+    c_label_var = StringVar()
+    c_label_var.set('Ready for conversion')
+    c_label = Label(textvariable=c_label_var)
+    c_label.pack(side='bottom', anchor='w')
+
+    #button convert
+    Button(root, text='Scan and Convert', bg='#F58D4B', font=('Times', 15),
+        width=70, command=convert).pack(pady='10', side='bottom')
+    root.mainloop()
+
+
+    
 
